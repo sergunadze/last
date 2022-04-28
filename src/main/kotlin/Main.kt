72fca7db377c301a0fa4d25cf8f56c6950a60e68
sergunadze <s_novikov@mail.ru>
2022-04-28 20:38:42 +0300

    test

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+var st: String = "PDDPEM MXWHL XJID XJID XJID DTQEHGC DTQEHGC MXWHL XJID DTQEHGC DTQEHGC MXWHL PDDPEM QUCO DTQEHGC MXWHL DTQEHGC MXWHL PDDPEM XJID MXWHL DTQEHGC DTQEHGC XJID XJID XJID MXWHL MXWHL MXWHL MXWHL DTQEHGC PDDPEM DTQEHGC DTQEHGC PDDPEM"
+    calculateWordStat(st)
+}
+
+fun calculateWordStat(input:String): String{
+    val c:List<String> =input.split(" ")
+    for (i in c) {
+
+//
+        }
+    return ""
+    }
+
